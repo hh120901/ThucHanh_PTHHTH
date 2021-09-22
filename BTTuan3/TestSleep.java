b82525c6337e9edd72285659484c9696dc43de6c
hh120901 <nguyenvanhau1209@gmail.com>
2021-09-22 14:48:12 +0700

    Tuan 3 Thread

---
+++ BTTuan3/TestSleep.java
@@ -0,0 +1,22 @@
+package BTTuan3;
+
+public class TestSleep extends Thread {
+    public void run() {
+        for (int i = 1; i < 5; i++) {
+            try {
+                Thread.sleep(500);// cho thread dung 0.5s
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+            System.out.println(i);
+        }
+    }
+ 
+    public static void main(String args[]) {
+        TestSleep t1 = new TestSleep();
+        TestSleep t2 = new TestSleep();
+ 
+        t1.start();
+        t2.start();
+    }
+}
---
+++ BTTuan3/ViduThread.java
@@ -0,0 +1,20 @@
+package BTTuan3;
+
+public class ViduThread extends Thread{
+    public void run(){
+        System.out.println("Thread is running...");
+        System.out.println(Thread.currentThread().getName());
+        System.out.println("Priority is: "+ Thread.currentThread().getPriority());
+    }
+    public static void main(String[] args) throws InterruptedException{
+        ViduThread t1 = new ViduThread();
+        ViduThread t2 = new ViduThread();
+
+        // Thread.sleep(2000);
+        t1.setPriority(MIN_PRIORITY);
+        t2.setPriority(MAX_PRIORITY);
+        t1.start();
+        t2.start();
+        
+    }
+}
